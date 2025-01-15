e5d68165c3ffc9a14c64bd672fb79f457b31a09a
Rayze <rayzeeeeeeee@gmail.com>
2025-01-15 22:43:53 +0100

    CoinExchange
    - Add research currency at the country

---
+++ CoinExchange.py
@@ -0,0 +1,405 @@
+# Dictionnary of the money in the world as a function of dollars
+Money = {
+    "USD": 1, #American dollars
+    "EUR": 0.9672, #Euro
+    "GBP": 0.8133, #UK pound
+    "INR": 86.3432, #Indian rupee
+    "CAD": 1.4314, #Canadian dollar
+    "AUD": 1.6024, #Australian dollar
+    "JPY": 156.1044, #Japanese yen
+    "CHF": 0.91, #Swiss franc
+    "RUB": 102.6768, #Russian Rubles
+    "NZD": 1.7726, #New Zealand dollar
+    "ZAR": 18.7693, #South African Rand
+    "BGN": 1.8913, #Bulgarian Lev
+    "SGD": 1.3636, #Singapore dollar
+    "HKD": 7.7847, #Hong Kong dollar
+    "SEK": 11.1129, #Swedish crown
+    "THB": 34.535, #Thai Baht
+    "HUF": 397.7833, #Hungarian Forint
+    "CNY": 7.3296, #Chinese Renminbi
+    "NOK": 11.3061, #Norwegian Koruna
+    "MXN": 20.4486, #Mexican peso
+    "BRL": 5.2743, #Brazilian Real
+    "KRW": 1326.47, #South Korean Won
+    "TRY": 28.6479, #Turkish Lira
+    "DKK": 7.4381, #Danish Krone
+    "PLN": 4.4682, #Polish Zloty
+    "RON": 4.9463, #Romanian Leu
+    "CZK": 24.588, #Czech Koruna
+    "HRK": 7.583, #Croatian Kuna
+    "ISK": 156.5, #Icelandic Krona
+    "PHP": 58.67, #Philippine Peso
+    "IDR": 15684.5, #Indonesian Rupiah
+    "MYR": 4.577, #Malaysian Ringgit
+    "VND": 24665.5, #Vietnamese Dong
+    "AED": 3.6725, #United Arab Emirates Dirham
+    "SAR": 3.75, #Saudi Riyal
+    "ILS": 3.727, #Israeli New Shekel
+    "ARS": 370.43, #Argentine Peso
+    "CLP": 936.75, #Chilean Peso
+    "COP": 4332.5, #Colombian Peso
+    "PEN": 3.865, #Peruvian Sol
+    "EGP": 30.885, #Egyptian Pound
+    "MAD": 10.945, #Moroccan Dirham
+    "NGN": 460.92, #Nigerian Naira
+    "KES": 136.5, #Kenyan Shilling
+    "UAH": 36.935, #Ukrainian Hryvnia
+    "TWD": 29.745, #New Taiwan Dollar
+    "KZT": 476.5, #Kazakhstani Tenge
+    "QAR": 3.64, #Qatari Rial
+    "KWD": 0.3075, #Kuwaiti Dinar
+    "BHD": 0.377, #Bahraini Dinar
+    "OMR": 0.385, #Omani Rial
+    "JOD": 0.709, #Jordanian Dinar
+    "LBP": 1507.5, #Lebanese Pound
+    "BYN": 2.725, #Belarusian Ruble
+    "RSD": 117.553, #Serbian Dinar
+    "UYU": 38.5, #Uruguayan Peso
+    "BWP": 13.45, #Botswana Pula
+    "GHS": 13.25, #Ghanaian Cedi
+    "TZS": 2485.5, #Tanzanian Shilling
+    "UGX": 3750.5, #Ugandan Shilling
+    "ZMW": 18.5, #Zambian Kwacha
+    "MWK": 1035.5, #Malawian Kwacha
+    "MUR": 44.5, #Mauritian Rupee
+    "ETB": 55.5, #Ethiopian Birr
+    "GMD": 57.5, #Gambian Dalasi
+    "SLL": 17000.5, #Sierra Leonean Leone
+    "LRD": 155.5, #Liberian Dollar
+    "XOF": 615.5, #CFA Franc BCEAO
+    "XAF": 615.5, #CFA Franc BEAC
+    "XPF": 119.3317, #CFP Franc
+    "DZD": 137.5, #Algerian Dinar
+    "TND": 3.058, #Tunisian Dinar
+    "LYD": 4.85, #Libyan Dinar
+    "SDG": 585.5, #Sudanese Pound
+    "SSP": 440.5, #South Sudanese Pound
+    "DJF": 178.5, #Djiboutian Franc
+    "SOS": 575.5, #Somali Shilling
+    "ERN": 15.5, #Eritrean Nakfa
+    "KMF": 492.5, #Comorian Franc
+    "SCR": 13.5, #Seychellois Rupee
+    "MGA": 4350.5, #Malagasy Ariary
+    "MZN": 64.5, #Mozambican Metical
+    "AOA": 825.5, #Angolan Kwanza
+    "BIF": 2050.5, #Burundian Franc
+    "RWF": 1050.5, #Rwandan Franc
+    "CDF": 2000.5, #Congolese Franc
+    "GNF": 8500.5, #Guinean Franc
+    "STN": 21.5, #São Tomé and Príncipe Dobra
+    "BMD": 1, #Bermudian Dollar
+    "KYD": 0.83, #Cayman Islands Dollar
+    "FJD": 2.28, #Fijian Dollar
+    "WST": 2.75, #Samoan Tala
+    "TOP": 2.35, #Tongan Pa'anga
+    "VUV": 120.5, #Vanuatu Vatu
+    "PGK": 3.75, #Papua New Guinean Kina
+    "SBD": 8.5, #Solomon Islands Dollar
+    "AWG": 1.8, #Aruban Florin
+    "CUC": 1, #Cuban Convertible Peso
+    "CUP": 24, #Cuban Peso
+    "DOP": 55.5, #Dominican Peso
+    "HTG": 130.5, #Haitian Gourde
+    "JMD": 150.5, #Jamaican Dollar
+    "BSD": 1, #Bahamian Dollar
+    "BBD": 2, #Barbadian Dollar
+    "TTD": 6.8, #Trinidad and Tobago Dollar
+    "XCD": 2.7, #East Caribbean Dollar
+    "GYD": 210.5, #Guyanese Dollar
+    "SRD": 37.5, #Surinamese Dollar
+    "BZD": 2, #Belize Dollar
+    "CRC": 620.5, #Costa Rican Colón
+    "NIO": 36.5, #Nicaraguan Córdoba
+    "HNL": 24.5, #Honduran Lempira
+    "SVC": 8.75, #Salvadoran Colón
+    "GTQ": 7.8, #Guatemalan Quetzal
+    "PAB": 1, #Panamanian Balboa
+    "PYG": 7000.5, #Paraguayan Guarani
+    "UYI": 40.5, #Uruguay Peso en Unidades Indexadas
+    "BOB": 6.9, #Bolivian Boliviano
+    "VEF": 248500, #Venezuelan Bolívar Fuerte
+    "AFN": 88.5, #Afghan Afghani
+    "AMD": 400.5, #Armenian Dram
+    "AZN": 1.7, #Azerbaijani Manat
+    "BAM": 1.95583, #Bosnia-Herzegovina Convertible Mark
+    "GEL": 2.75, #Georgian Lari
+    "KGS": 84.5, #Kyrgyzstani Som
+    "MDL": 19.5, #Moldovan Leu
+    "TJS": 11.5, #Tajikistani Somoni
+    "TMT": 3.5, #Turkmenistani Manat
+    "UZS": 11250.5, #Uzbekistan Som
+    "MNT": 3350.5, #Mongolian Tugrik
+    "NPR": 130.5, #Nepalese Rupee
+    "PKR": 280.5, #Pakistani Rupee
+    "LKR": 360.5, #Sri Lankan Rupee
+    "BDT": 105.5, #Bangladeshi Taka
+    "BTN": 80.5, #Bhutanese Ngultrum
+    "MVR": 15.5, #Maldivian Rufiyaa
+    "KHR": 4100.5, #Cambodian Riel
+    "LAK": 17000.5, #Lao Kip
+    "MMK": 2100.5, #Myanmar Kyat
+    "BND": 1.35, #Brunei Dollar
+    "KPW": 900.5, #North Korean Won
+    "VES": 248500, #Venezuelan Bolívar Soberano
+    "SYP": 2500.5, #Syrian Pound
+    "YER": 250.5, #Yemeni Rial
+    "IQD": 1460.5, #Iraqi Dinar
+    "IRR": 42000.5, #Iranian Rial
+    "SZL": 17.5, #Swazi Lilangeni
+    "MOP": 8.05, #Macanese Pataca
+    "ANG": 1.8, #Netherlands Antillean Guilder
+    "TWD": 29.745, #New Taiwan Dollar
+    "BMD": 1, #Bermudian Dollar
+    "KYD": 0.83, #Cayman Islands Dollar
+    "FJD": 2.28, #Fijian Dollar
+    "WST": 2.75, #Samoan Tala
+    "TOP": 2.35, #Tongan Pa'anga
+    "VUV": 120.5, #Vanuatu Vatu
+    "PGK": 3.75, #Papua New Guinean Kina
+    "SBD": 8.5, #Solomon Islands Dollar
+    "AWG": 1.8, #Aruban Florin
+    "CUC": 1, #Cuban Convertible Peso
+    "CUP": 24, #Cuban Peso
+    "DOP": 55.5, #Dominican Peso
+    "HTG": 130.5, #Haitian Gourde
+    "JMD": 150.5, #Jamaican Dollar
+    "BSD": 1, #Bahamian Dollar
+    "BBD": 2, #Barbadian Dollar
+    "TTD": 6.8, #Trinidad and Tobago Dollar
+    "XCD": 2.7, #East Caribbean Dollar
+    "GYD": 210.5, #Guyanese Dollar
+    "SRD": 37.5, #Surinamese Dollar
+    "BZD": 2, #Belize Dollar
+    "CRC": 620.5, #Costa Rican Colón
+    "NIO": 36.5, #Nicaraguan Córdoba
+    "HNL": 24.5, #Honduran Lempira
+    "SVC": 8.75, #Salvadoran Colón
+    "GTQ": 7.8, #Guatemalan Quetzal
+    "PAB": 1, #Panamanian Balboa
+    "PYG": 7000.5, #Paraguayan Guarani
+    "UYI": 40.5, #Uruguay Peso en Unidades Indexadas
+    "BOB": 6.9, #Bolivian Boliviano
+    "VEF": 248500, #Venezuelan Bolívar Fuerte
+    "AFN": 88.5, #Afghan Afghani
+    "AMD": 400.5, #Armenian Dram
+    "AZN": 1.7, #Azerbaijani Manat
+    "BAM": 1.95583, #Bosnia-Herzegovina Convertible Mark
+    "GEL": 2.75, #Georgian Lari
+    "KGS": 84.5, #Kyrgyzstani Som
+    "MDL": 19.5, #Moldovan Leu
+    "TJS": 11.5, #Tajikistani Somoni
+    "TMT": 3.5, #Turkmenistani Manat
+    "UZS": 11250.5, #Uzbekistan Som
+    "MNT": 3350.5, #Mongolian Tugrik
+    "NPR": 130.5, #Nepalese Rupee
+    "PKR": 280.5, #Pakistani Rupee
+    "LKR": 360.5, #Sri Lankan Rupee
+    "BDT": 105.5, #Bangladeshi Taka
+    "BTN": 80.5, #Bhutanese Ngultrum
+    "MVR": 15.5, #Maldivian Rufiyaa
+    "KHR": 4100.5, #Cambodian Riel
+    "LAK": 17000.5, #Lao Kip
+    "MMK": 2100.5, #Myanmar Kyat
+    "BND": 1.35, #Brunei Dollar
+    "KPW": 900.5, #North Korean Won
+    "VES": 248500, #Venezuelan Bolívar Soberano
+    "SYP": 2500.5, #Syrian Pound
+    "YER": 250.5, #Yemeni Rial
+    "IQD": 1460.5, #Iraqi Dinar
+    "IRR": 42000.5, #Iranian Rial
+    "SZL": 17.5, #Swazi Lilangeni
+    "MOP": 8.05, #Macanese Pataca
+    "ANG": 1.8, #Netherlands Antillean Guilder
+}
+
+# Dictionnary of the currency of the country in the world
+Currency_of_country = {
+    "United States": "American dollars - USD",
+    "Eurozone": "Euro - EUR",
+    "United Kingdom": "UK pound - GBP",
+    "India": "Indian rupee - INR",
+    "Canada": "Canadian dollar - CAD",
+    "Australia": "Australian dollar - AUD",
+    "Japan": "Japanese yen - JPY",
+    "Switzerland": "Swiss franc - CHF",
+    "Russia": "Russian Rubles - RUB",
+    "New Zealand": "New Zealand dollar - NZD",
+    "South Africa": "South African Rand - ZAR",
+    "Bulgaria": "Bulgarian Lev - BGN",
+    "Singapore": "Singapore dollar - SGD",
+    "Hong Kong": "Hong Kong dollar - HKD",
+    "Sweden": "Swedish crown - SEK",
+    "Thailand": "Thai Baht - THB",
+    "Hungary": "Hungarian Forint - HUF",
+    "China": "Chinese Renminbi - CNY",
+    "Norway": "Norwegian Koruna - NOK",
+    "Mexico": "Mexican peso - MXN",
+    "Brazil": "Brazilian Real - BRL",
+    "South Korea": "South Korean Won - KRW",
+    "Turkey": "Turkish Lira - TRY",
+    "Denmark": "Danish Krone - DKK",
+    "Poland": "Polish Zloty - PLN",
+    "Romania": "Romanian Leu - RON",
+    "Czech Republic": "Czech Koruna - CZK",
+    "Croatia": "Croatian Kuna - HRK",
+    "Iceland": "Icelandic Krona - ISK",
+    "Philippines": "Philippine Peso - PHP",
+    "Indonesia": "Indonesian Rupiah - IDR",
+    "Malaysia": "Malaysian Ringgit - MYR",
+    "Vietnam": "Vietnamese Dong - VND",
+    "United Arab Emirates": "United Arab Emirates Dirham - AED",
+    "Saudi Arabia": "Saudi Riyal - SAR",
+    "Israel": "Israeli New Shekel - ILS",
+    "Argentina": "Argentine Peso - ARS",
+    "Chile": "Chilean Peso - CLP",
+    "Colombia": "Colombian Peso - COP",
+    "Peru": "Peruvian Sol - PEN",
+    "Egypt": "Egyptian Pound - EGP",
+    "Morocco": "Moroccan Dirham - MAD",
+    "Nigeria": "Nigerian Naira - NGN",
+    "Kenya": "Kenyan Shilling - KES",
+    "Ukraine": "Ukrainian Hryvnia - UAH",
+    "Taiwan": "New Taiwan Dollar - TWD",
+    "Kazakhstan": "Kazakhstani Tenge - KZT",
+    "Qatar": "Qatari Rial - QAR",
+    "Kuwait": "Kuwaiti Dinar - KWD",
+    "Bahrain": "Bahraini Dinar - BHD",
+    "Oman": "Omani Rial - OMR",
+    "Jordan": "Jordanian Dinar - JOD",
+    "Lebanon": "Lebanese Pound - LBP",
+    "Belarus": "Belarusian Ruble - BYN",
+    "Serbia": "Serbian Dinar - RSD",
+    "Uruguay": "Uruguayan Peso - UYU",
+    "Botswana": "Botswana Pula - BWP",
+    "Ghana": "Ghanaian Cedi - GHS",
+    "Tanzania": "Tanzanian Shilling - TZS",
+    "Uganda": "Ugandan Shilling - UGX",
+    "Zambia": "Zambian Kwacha - ZMW",
+    "Malawi": "Malawian Kwacha - MWK",
+    "Mauritius": "Mauritian Rupee - MUR",
+    "Ethiopia": "Ethiopian Birr - ETB",
+    "Gambia": "Gambian Dalasi - GMD",
+    "Sierra Leone": "Sierra Leonean Leone - SLL",
+    "Liberia": "Liberian Dollar - LRD",
+    "CFA Franc BCEAO": "CFA Franc BCEAO - XOF",
+    "CFA Franc BEAC": "CFA Franc BEAC - XAF",
+    "CFP Franc": "CFP Franc - XPF",
+    "Algeria": "Algerian Dinar - DZD",
+    "Tunisia": "Tunisian Dinar - TND",
+    "Libya": "Libyan Dinar - LYD",
+    "Sudan": "Sudanese Pound - SDG",
+    "South Sudan": "South Sudanese Pound - SSP",
+    "Djibouti": "Djiboutian Franc - DJF",
+    "Somalia": "Somali Shilling - SOS",
+    "Eritrea": "Eritrean Nakfa - ERN",
+    "Comoros": "Comorian Franc - KMF",
+    "Seychelles": "Seychellois Rupee - SCR",
+    "Madagascar": "Malagasy Ariary - MGA",
+    "Mozambique": "Mozambican Metical - MZN",
+    "Angola": "Angolan Kwanza - AOA",
+    "Burundi": "Burundian Franc - BIF",
+    "Rwanda": "Rwandan Franc - RWF",
+    "Congo": "Congolese Franc - CDF",
+    "Guinea": "Guinean Franc - GNF",
+    "São Tomé and Príncipe": "São Tomé and Príncipe Dobra - STN",
+    "Bermuda": "Bermudian Dollar - BMD",
+    "Cayman Islands": "Cayman Islands Dollar - KYD",
+    "Fiji": "Fijian Dollar - FJD",
+    "Samoa": "Samoan Tala - WST",
+    "Tonga": "Tongan Pa'anga - TOP",
+    "Vanuatu": "Vanuatu Vatu - VUV",
+    "Papua New Guinea": "Papua New Guinean Kina - PGK",
+    "Solomon Islands": "Solomon Islands Dollar - SBD",
+    "Aruba": "Aruban Florin - AWG",
+    "Cuba": "Cuban Convertible Peso - CUC",
+    "Cuba": "Cuban Peso - CUP",
+    "Dominican Republic": "Dominican Peso - DOP",
+    "Haiti": "Haitian Gourde - HTG",
+    "Jamaica": "Jamaican Dollar - JMD",
+    "Bahamas": "Bahamian Dollar - BSD",
+    "Barbados": "Barbadian Dollar - BBD",
+    "Trinidad and Tobago": "Trinidad and Tobago Dollar - TTD",
+    "East Caribbean": "East Caribbean Dollar - XCD",
+    "Guyana": "Guyanese Dollar - GYD",
+    "Suriname": "Surinamese Dollar - SRD",
+    "Belize": "Belize Dollar - BZD",
+    "Costa Rica": "Costa Rican Colón - CRC",
+    "Nicaragua": "Nicaraguan Córdoba - NIO",
+    "Honduras": "Honduran Lempira - HNL",
+    "El Salvador": "Salvadoran Colón - SVC",
+    "Guatemala": "Guatemalan Quetzal - GTQ",
+    "Panama": "Panamanian Balboa - PAB",
+    "Paraguay": "Paraguayan Guarani - PYG",
+    "Uruguay": "Uruguay Peso en Unidades Indexadas - UYI",
+    "Bolivia": "Bolivian Boliviano - BOB",
+    "Venezuela": "Venezuelan Bolívar Fuerte - VEF",
+    "Afghanistan": "Afghan Afghani - AFN",
+    "Armenia": "Armenian Dram - AMD",
+    "Azerbaijan": "Azerbaijani Manat - AZN",
+    "Bosnia and Herzegovina": "Bosnia-Herzegovina Convertible Mark - BAM",
+    "Georgia": "Georgian Lari - GEL",
+    "Kyrgyzstan": "Kyrgyzstani Som - KGS",
+    "Moldova": "Moldovan Leu - MDL",
+    "Tajikistan": "Tajikistani Somoni - TJS",
+    "Turkmenistan": "Turkmenistani Manat - TMT",
+    "Uzbekistan": "Uzbekistan Som - UZS",
+    "Mongolia": "Mongolian Tugrik - MNT",
+    "Nepal": "Nepalese Rupee - NPR",
+    "Pakistan": "Pakistani Rupee - PKR",
+    "Sri Lanka": "Sri Lankan Rupee - LKR",
+    "Bangladesh": "Bangladeshi Taka - BDT",
+    "Bhutan": "Bhutanese Ngultrum - BTN",
+    "Maldives": "Maldivian Rufiyaa - MVR",
+    "Cambodia": "Cambodian Riel - KHR",
+    "Laos": "Lao Kip - LAK",
+    "Myanmar": "Myanmar Kyat - MMK",
+    "Brunei": "Brunei Dollar - BND",
+    "North Korea": "North Korean Won - KPW",
+    "Venezuela": "Venezuelan Bolívar Soberano - VES",
+    "Syria": "Syrian Pound - SYP",
+    "Yemen": "Yemeni Rial - YER",
+    "Iraq": "Iraqi Dinar - IQD",
+    "Iran": "Iranian Rial - IRR",
+    "Swaziland": "Swazi Lilangeni - SZL",
+    "Macau": "Macanese Pataca - MOP",
+    "Netherlands Antilles": "Netherlands Antillean Guilder - ANG",
+}
+
+# Start function
+def Start():
+    Choice_option = input("Entry your choice (Convert / Research): ") #Entry the choice of the option
+
+    #If "Convert", bring in the Convert function
+    if (Choice_option=="Convert"):
+        Convert()
+    
+    #If "Research", bring in the Currency function
+    if (Choice_option=="Research"):
+        Currency_research()
+
+# Convert function
+def Convert():
+    #Entry for convert money
+    base_currency = input("Entry the base currency :")
+    sum_entry = int(input("Entry the sum of exchange :"))
+    target_currency = input("Entry the target currency :")
+    
+    base = Money[base_currency] #Base is equal to the currency in the "Money" dictionary
+    target = Money[target_currency] #Target is equal to the currency in the "Money" dictionary
+    
+    exchange = ((target * Money["USD"]) / base) * sum_entry #Currency conversion
+    exchange_format = format(exchange, '.2f') #Format of the result
+
+    print(f"{sum_entry} {base_currency} is equal to {exchange_format} {target_currency}") #Result of the conversion
+    Start() #Bring to "Start()"
+
+# Currency research function
+def Currency_research():
+    Research = input("Entry the country for discover her currency :") #Entry for research currency of country
+    Country = Currency_of_country[Research] # Country is equal to the Research in the "Currency_of_country" dictionnary
+    print(f"The currency of the {Research} is {Country}") #Result of the search
+
+    Start()#Bring to "Start()"
+
+Start()
